31253332cbd99a4b75362bb6f5d5d552a71feca0
joe <joe.ulica@gmail.com>
2022-08-24 13:35:32 -0400

    add index action

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,39 @@
+<script>
+import { assertExpressionStatement } from '@babel/types';
+import axios from "axios"
+
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      pets: []
+    };
+  },
+  created: function () {
+    this.getPets()
+  },
+  methods: {
+    getPets: function () {
+      console.log("getting pets")
+      axios.get("http://localhost:3000/pets.json").then(response => {
+        this.pets = response.data
+        console.log(this.pets)
+      })
+    }
+  },
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div v-for="pet in pets">
+      {{ pet.name }} <br />
+      <button v-on:click="showPet">more info</button>
+      <hr />
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style>
+</style>
