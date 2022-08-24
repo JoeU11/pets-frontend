46ed31adb659a5110ac94e8d67abe6804710f2a6
joe <joe.ulica@gmail.com>
2022-08-24 13:44:21 -0400

    add show pet action

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,8 @@ export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
-      pets: []
+      pets: [],
+      currentPet: {}
     };
   },
   created: function () {
@@ -19,6 +20,11 @@ export default {
         this.pets = response.data
         console.log(this.pets)
       })
+    },
+    showPet: function (pet) {
+      console.log("showing pet")
+      this.currentPet = pet
+      document.querySelector("#details").showModal()
     }
   },
 };
@@ -29,11 +35,22 @@ export default {
     <h1>{{ message }}</h1>
     <div v-for="pet in pets">
       {{ pet.name }} <br />
-      <button v-on:click="showPet">more info</button>
+      <button v-on:click="showPet(pet)">more info</button>
       <hr />
     </div>
   </div>
+
+  <dialog id="details">
+    <form model="dialog">
+      name: {{ currentPet.name }} <br />
+      animal type: {{ currentPet.animal }} <br />
+      age: {{ currentPet.age }} <br />
+      <button>close</button>
+    </form>
+  </dialog>
 </template>
+
+
 
 <style>
 </style>
